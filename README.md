16760e4c780f2616058001f15f45f5f5878d728c
randaaaaaaa <73005412+randaaaaaaa@users.noreply.github.com>
2022-09-01 09:10:01 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+# Weather-Journal App Project
+overview:
+
+I set up my environment by unstalling Node and made sure it  works  and included in my server.js file.
+Added POST and GET routes to make sure data transfere from server sid.
+Got API credentials  from OpenWeatherMap  website
+Created async functions to fetch weather data and stor it in a variable.
+
+Resources:
+https://dev.to/gbudjeakp/how-to-connect-your-client-side-to-your-server-side-using-node-and-express-2i71
+https://openweathermap.org/current#zip
+https://bithacker.dev/fetch-weather-openweathermap-api-javascript
+https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toDateString
--- server.js
+++ server.js
@@ -1,22 +1,52 @@
 // Setup empty JS object to act as endpoint for all routes
-// Express to run server and routes
+projectData = {};
+
+// Require Express to run server and routes
+const express = require("express");
+
+const port = 3001
+
+const hostName = "127.0.0.1";
+
+//manage cross-margin resources 
+const cors = require("cors")
+
+//body parser allow backend to acsess Json data sent from the client using request.body
+const bodyParser = require("body-parser")
+
 
 // Start up an instance of app
+const app = express() 
 
-/* Dependencies */
 /* Middleware*/
-
 //Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // Cors for cross origin allowance
+app.use(cors())
+
+
+//defining post rout
+app.post("/saveData", (req, res) => {
+    projectData.temp = req.body.temp
+    projectData.content = req.body.content
+    projectData.date = req.body.date
+    res.send();
+    console.log(projectData);
+})
+
+//defining get rout
+const getData = (req, res) => res.send(projectData);
+app.get("/allData", getData);
 
 // Initialize the main project folder
+app.use(express.static('website'));
 
-// Spin up the server
-// Callback to debug
 
-// Initialize all route with a callback function
+// Setup Server
 
-// Callback function to complete GET '/all'
+const testing = () => 
+console.log(`Your server is running on http://127.0.0.1:${port}`);
 
-// Post Route
-  
+app.listen(port, testing);
